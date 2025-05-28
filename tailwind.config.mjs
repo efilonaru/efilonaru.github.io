f0f34cab45e4793edf5a90c595d86ddbcfa2dfb8
efilonaru <naufalyasir5467@gmail.com>
2025-05-28 11:32:47 +0700

    fix: handle no blog post

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+      },
       typography: {
         DEFAULT: {
           css: {
